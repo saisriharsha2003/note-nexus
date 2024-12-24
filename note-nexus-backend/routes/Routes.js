19b1a0b1e057bb25e05d0ff15aad4a4dd9921f6b
Rankela Sai Sri Harsha <113669866+saisriharsha2003@users.noreply.github.com>
2024-12-24 09:04:24 +0000

    Updated docekr compose to rebuild after changes

--- note-nexus-backend/routes/Routes.js
+++ note-nexus-backend/routes/Routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { signup, signin, add_note, view_notes, view_note_by_id, edit_note, delete_note, getUserProfile, updatePassword, updateUserProfile } = require('../controllers/userController');
 const router = express.Router();
 
+router.route('/').get((req, res) => {
+    res.send('Welcome to the NoteNexus!');
+});
 router.route('/register').post(signup);
 router.route('/login').post(signin);
 router.route('/add-note').post(add_note);
--- note-nexus-frontend/src/components/Home.jsx
+++ note-nexus-frontend/src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
       <div className="bg1">
         <div className="container" style={{ background: "black", opacity: 0.9 }}>
           <p className="text-4xl font-semibold text-[#CCBA78] flex justify-center items-center">
-            Welcome to <span className="homep">NoteNexus</span>
+            Welcome to<span className="homep">NoteNexus</span>
           </p>
         </div>
       </div>
