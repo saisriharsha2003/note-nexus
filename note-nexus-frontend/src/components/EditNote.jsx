ca380ab7655d3e38afb10a12e1c24d4a49f2900e
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-03-11 22:24:21 +0530

    updated editnote component

--- note-nexus-frontend/src/components/EditNote.jsx
+++ note-nexus-frontend/src/components/EditNote.jsx
@@ -7,6 +7,9 @@ import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import { motion } from "framer-motion";
+import { FaHeading, FaLock, FaGlobe } from "react-icons/fa";
+import "../assets/styles/editnote.css";
 
 const EditNote = () => {
   const { id } = useParams();
@@ -45,9 +48,7 @@ const EditNote = () => {
           owner: response.data.note.owner_username,
           visibility: response.data.note.visibility || "private",
         });
-        console.log(formData.owner);
       } catch (error) {
-        console.error("Error fetching note:", error);
         toast.error("Failed to load the note.", {
           position: "top-right",
           autoClose: 1500,
@@ -56,23 +57,16 @@ const EditNote = () => {
         setLoading(false);
       }
     };
-
     fetchNote();
   }, [id]);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const handleContentChange = (value) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      content: value,
-    }));
+    setFormData((prevData) => ({ ...prevData, content: value }));
   };
 
   const handleVisibilityToggle = () => {
@@ -94,110 +88,143 @@ const EditNote = () => {
         autoClose: 1500,
       });
       setTimeout(() => {
-        toast.success("Redirecting to View Notes...", {
-          position: "top-right",
-        });
-        setTimeout(() => {
-          navigate("/view-notes");
-        }, 1000);
+        navigate("/view-notes");
       }, 2000);
     } catch (error) {
       toast.error("Failed to save note.", {
         position: "top-right",
         autoClose: 1500,
       });
-      console.error("Error saving note:", error);
     }
   };
 
   return (
     <div>
       <Nav />
-      <div className="bg1">
-        <div className="container">
-          <h2 className="title">Edit Note</h2>
-          {loading ? (
-            <p>Loading note...</p>
-          ) : (
-            <form>
-              <div className="input-box1 w-full mb-4">
-                <label className="text-[#CCBA78] pb-2 block mb-2 font-semibold">
-                  Title
-                </label>
-                <input
-                  type="text"
-                  name="title"
-                  value={formData.title}
-                  onChange={handleChange}
-                  className="w-full p-2 border"
-                />
-              </div>
-              <div className="input-box1 w-full pb-4">
-                <label className="text-[#CCBA78] block mb-2 font-semibold">
-                  Content
-                </label>
-                <ReactQuill
-                  value={formData.content}
-                  modules={modules}
-                  onChange={handleContentChange}
-                  className="h-48 mb-14 text-white"
-                />
-              </div>
-              <div className="text-[#CCBA78] mb-4">
-                <span className="font-semibold">Last Edited By:</span>{" "}
-                <span className="text-white">{formData.lastEditedBy}</span>
-              </div>
-              <div className="mb-4">
-                {formData.owner === user ? (
-                  <div>
-                    <label className="text-[#CCBA78] font-semibold">
-                      Visibility:{" "}
-                    </label>
-                    <button
-                      type="button"
-                      onClick={handleVisibilityToggle}
-                      className={`px-4 py-2 ${
-                        formData.visibility === "public"
-                          ? "bg-green-500 text-white"
-                          : "bg-blue-500 text-white"
-                      } rounded`}
-                      title="Click to toggle between Public and Private"
-                    >
-                      {formData.visibility === "public" ? "Public" : "Private"}
-                    </button>
-                    <p className="text-sm text-gray-300 mt-1">
-                      Click the button to change visibility.
-                    </p>
-                  </div>
-                ) : (
-                  " "
-                )}
-              </div>
+      <motion.div
+        className="bg1"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+      >
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="edit-note-container"
+        >
+          <motion.div
+            className="edit-box"
+            initial={{ scale: 0.9 }}
+            animate={{ scale: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h2 className="title">Edit Note</h2>
+            {loading ? (
+              <p className="loading-text">Loading note...</p>
+            ) : (
+              <motion.form
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.8 }}
+              >
+                <motion.div
+                  className="input-box1 w-full mb-4"
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <label className="label">Title</label>
+                  <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    className="title-input"
+                  />
+                </motion.div>
+                <motion.div
+                  className="input-box1 w-full pb-4"
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <label className="label">Content</label>
+                  <ReactQuill
+                    value={formData.content}
+                    modules={modules}
+                    onChange={handleContentChange}
+                    className="content-editor"
+                    whileHover={{ scale: 1.1 }}
+                  />
+                </motion.div>
+                <motion.div
+                  className="visibility-options"
+                  initial={{ scale: 0.8 }}
+                  animate={{ scale: 1 }}
+                >
+                  <motion.label
+                    whileHover={{ scale: 1.1 }}
+                    className={`visibility-label ${
+                      formData.visibility === "private" ? "selected" : ""
+                    }`}
+                  >
+                    <input
+                      type="radio"
+                      name="visibility"
+                      value="private"
+                      checked={formData.visibility === "private"}
+                      onChange={handleChange}
+                    />
+                    <FaLock className="visibility-icon" /> Private
+                  </motion.label>
 
-              <div className="flexcenter">
-                <div className="button w-full pr-5">
-                  <button
+                  <motion.label
+                    whileHover={{ scale: 1.1 }}
+                    className={`visibility-label ${
+                      formData.visibility === "public" ? "selected" : ""
+                    }`}
+                  >
+                    <input
+                      type="radio"
+                      name="visibility"
+                      value="public"
+                      checked={formData.visibility === "public"}
+                      onChange={handleChange}
+                    />
+                    <FaGlobe className="visibility-icon" /> Public
+                  </motion.label>
+                </motion.div>
+                <motion.div
+                  className="button-container"
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <motion.button
+                    whileHover={{ scale: 1.001 }}
+                    whileTap={{ scale: 0.9 }}
                     type="button"
                     onClick={handleSave}
-                    className="aButton"
+                    className="save-button"
                   >
                     Save Note
-                  </button>
-                </div>
-                <div className="button w-full pl-5">
-                  <button
+                  </motion.button>
+                  <motion.button
+                    whileHover={{ scale: 1.001 }}
+                    whileTap={{ scale: 0.9 }}
                     type="button"
                     onClick={() => navigate("/view-notes")}
-                    className="aButton"
+                    className="cancel-button"
                   >
                     Cancel
-                  </button>
-                </div>
-              </div>
-            </form>
-          )}
-        </div>
-      </div>
+                  </motion.button>
+                </motion.div>
+              </motion.form>
+            )}
+          </motion.div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
