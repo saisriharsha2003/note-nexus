48435b2693d8c79647fcfb59d90c566e93f6b072
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-03-13 23:17:26 +0530

    updated notes model

--- note-nexus-backend/models/User.js
+++ note-nexus-backend/models/User.js
@@ -33,6 +33,14 @@ const UserSchema = new mongoose.Schema(
       required: [true, 'Please provide a password'],
       minlength: [6, 'Password must be at least 6 characters long'],
     },
+    resetPasswordCode: {
+      type: String,
+      default: null,
+    },
+    resetPasswordExpires: {
+      type: Date,
+      default: null,
+    },
   },
   { timestamps: true }
 );
