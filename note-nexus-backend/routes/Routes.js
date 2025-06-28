dbeaf210979aa112a80bf4f967bca6012af0fd78
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-06-28 21:09:44 +0530

    updated routes for updated logic

--- note-nexus-backend/routes/Routes.js
+++ note-nexus-backend/routes/Routes.js
@@ -1,38 +1,43 @@
 import express from 'express';
-import { 
-  signup, 
-  signin, 
-  add_note, 
-  view_notes, 
-  view_note_by_id, 
-  edit_note, 
-  delete_note, 
-  getUserProfile, 
-  updatePassword, 
-  updateUserProfile, 
-  resetPassword, 
-  verifyCode, 
-  newPassword 
-} from '../controllers/userController.js';  
+import {
+  signup,
+  signin,
+  add_note,
+  view_notes,
+  view_note_by_id,
+  edit_note,
+  delete_note,
+  getUserProfile,
+  updatePassword,
+  updateUserProfile,
+  resetPassword,
+  verifyCode,
+  newPassword,
+  getNotifications,
+  resetNotificationCount
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.route('/').get((req, res) => {
-    res.send('Welcome to the NoteNexus!');
+  res.send('Welcome to the NoteNexus!');
 });
 
-router.route('/register').post(signup);
-router.route('/login').post(signin);
-router.route('/add-note').post(add_note);
-router.route('/view-notes').get(view_notes);
-router.route('/note/:noteid').get(view_note_by_id);
-router.route('/edit-note').put(edit_note);
-router.route("/delete-note/:id").delete(delete_note);
-router.route('/profile/:uname').get(getUserProfile);
-router.route('/update-password/:uname').put(updatePassword);
-router.route('/update-profile/:uname').put(updateUserProfile);
-router.route("/reset-password").post(resetPassword);
-router.route("/verify-code").post(verifyCode);
-router.route("/new-password").put(newPassword);
+router.post('/register', signup);
+router.post('/login', signin);
+router.post('/add-note', add_note);
+router.get('/view-notes', view_notes);
+router.get('/note/:noteid', view_note_by_id);
+router.put('/edit-note', edit_note);
+router.delete('/delete-note/:id', delete_note);
+router.get('/profile/:uname', getUserProfile);
+router.put('/update-password/:uname', updatePassword);
+router.put('/update-profile/:uname', updateUserProfile);
+router.post('/reset-password', resetPassword);
+router.post('/verify-code', verifyCode);
+router.put('/new-password', newPassword);
 
-export default router;  
+router.get('/notifications/:username', getNotifications);
+router.post('/notifications/reset/:username', resetNotificationCount);
+
+export default router;
