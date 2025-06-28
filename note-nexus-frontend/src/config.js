d84f474756f1556e98b7e5abba9206c1690c77a2
Rankela Sai Sri Harsha <saisriharsha.r@gmail.com>
2025-06-28 21:10:06 +0530

    added required packages for websocket and redis in frontend

--- note-nexus-frontend/src/config.js
+++ note-nexus-frontend/src/config.js
@@ -1,6 +1,6 @@
-// export const BASE_URL = "http://localhost:8080" 
+export const BASE_URL = "http://localhost:8081" 
 
-export const BASE_URL = "https://note-nexus.onrender.com" 
+// export const BASE_URL = "https://note-nexus.onrender.com" 
 
 // export const BASE_URL = "http://note-nexus-backend-service:8080";
 
